4402101b66a74666801a7b1d3bc94618376c62e6
DeanTevin <Deantevinn@gmail.com>
2018-12-12 16:29:16 +0700

    chinese is deleted

--- LocalizeData.java
+++ LocalizeData.java
@@ -3,8 +3,6 @@ case "1" : setEnglish(); run();
 break;
 case "2" : setFrance(); run();
 break;
-case "3" : setChinese(); run();
-break;
-case "4" : setItalian(); run();
+case "3" : setItalian(); run();
 break;
 }
